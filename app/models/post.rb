5f15eb2311cb2044425846ca3be9b534933b1698
tavo <gasb_150@hotmail.com>
2021-02-02 16:09:47 -0500

    add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
+
+    validates: :title, precense: true
+    validates: :body, precense: true
+    validates :title, presence: true, length: { maximun: 20 }
 end
